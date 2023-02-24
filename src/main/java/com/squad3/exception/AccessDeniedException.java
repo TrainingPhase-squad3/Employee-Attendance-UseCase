5e27caa3604948593229dcb3a1e1f99ff6148569
vamsha591 <65599757+vamsha591@users.noreply.github.com>
2023-02-24 18:03:40 +0530

    Update AccessDeniedException.java

--- src/main/java/com/squad3/exception/AccessDeniedException.java
+++ src/main/java/com/squad3/exception/AccessDeniedException.java
@@ -1,1 +1,15 @@
+public class AccessDeniedException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public AccessDeniedException(String message) {
+		super(message);
+	}
+
+	public AccessDeniedException() {
+		super();
+	}
+}
 
