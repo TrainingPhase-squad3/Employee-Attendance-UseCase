08501e8481a22b54a25c9427b676071df059a48b
vamsha591 <65599757+vamsha591@users.noreply.github.com>
2023-02-24 18:07:45 +0530

    Update NotImplementedException.java

--- src/main/java/com/squad3/exception/NotImplementedException.java
+++ src/main/java/com/squad3/exception/NotImplementedException.java
@@ -1,1 +1,16 @@
+package com.squad3.exception;
+
+public class NotImplementedException extends RuntimeException {
+/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public NotImplementedException(String message) {
+		super(message);
+	}
+
+	public NotImplementedException() {
+		super();
+	}
+}
 
