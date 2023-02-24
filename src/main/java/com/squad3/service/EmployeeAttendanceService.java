39bd330a15466be9e1a2fd7044ea3c3ebd2c26d0
CHAITRA P SHETTY <64059838+Chaitrashetty2001@users.noreply.github.com>
2023-02-24 17:17:32 +0530

    Update EmployeeAttendanceService.java

--- src/main/java/com/squad3/service/EmployeeAttendanceService.java
+++ src/main/java/com/squad3/service/EmployeeAttendanceService.java
@@ -7,5 +7,5 @@ import com.squad3.response.SwipingResponse;
 
 public interface EmployeeAttendanceService {
 
-	List<SwipingResponse> employeeAttendenceHistory(long adminId, long employeeId, String fromDate, String toDate);
+	
 }
