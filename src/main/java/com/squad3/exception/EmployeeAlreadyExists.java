3ccbe82c772de609604ddaecfaa94c4537a8b01b
vamsha591 <65599757+vamsha591@users.noreply.github.com>
2023-02-24 18:04:48 +0530

    Update EmployeeAlreadyExists.java

--- src/main/java/com/squad3/exception/EmployeeAlreadyExists.java
+++ src/main/java/com/squad3/exception/EmployeeAlreadyExists.java
@@ -1,5 +1,15 @@
 package com.squad3.exception;
 
 public class EmployeeAlreadyExists extends RuntimeException {
+/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public EmployeeAlreadyExists(String message) {
+		super(message);
+	}
 
+	public EmployeeAlreadyExists() {
+		super();
+	}
 }
