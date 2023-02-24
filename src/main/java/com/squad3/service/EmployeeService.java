c9f0d2c5dacf48508904d5d08dd77afa546052af
Manoj Kumar M K <114489075+Manoj2208@users.noreply.github.com>
2023-02-24 17:11:19 +0530

    Update EmployeeService.java

--- src/main/java/com/squad3/service/EmployeeService.java
+++ src/main/java/com/squad3/service/EmployeeService.java
@@ -3,9 +3,7 @@ package com.squad3.service;
 public interface EmployeeService {
 	
 
-ResponseStructure adminLogIn(String email, String password);
 
-	ResponseStructure adminLogOut(String email);
 
 
 
