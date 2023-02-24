42e8bf3f42c6ce8ea994d93c318ca832abe55c21
vamsha591 <65599757+vamsha591@users.noreply.github.com>
2023-02-24 18:28:09 +0530

    Update EmployeeService.java

--- src/main/java/com/squad3/service/EmployeeService.java
+++ src/main/java/com/squad3/service/EmployeeService.java
@@ -1,10 +1,11 @@
 package com.squad3.service;
 
 public interface EmployeeService {
+Response save(EmployeeDto employeedto);
 	
 ResponseStructure adminLogIn(String email, String password);
 
-	ResponseStructure adminLogOut(String email);
+ResponseStructure adminLogOut(String email);
 
 
 
